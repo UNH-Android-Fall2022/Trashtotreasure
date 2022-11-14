3693529b305a178a9742ee92dc592099b952a696
Neha-reddy <nehareddylomada@gmail.com>
2022-11-14 22:40:43 +0530

    mobile navigation changes

--- app/src/main/java/com/newhaven/trashtotreasure/MainActivity.kt
+++ app/src/main/java/com/newhaven/trashtotreasure/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.newhaven.trashtotreasure
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.newhaven.trashtotreasure.home.HomeActivity
+import com.newhaven.trashtotreasure.home.TrashToTreasure
 import com.newhaven.trashtotreasure.login.LoginActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +18,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         CoroutineScope(Dispatchers.Main).launch {
             delay(3000)
-            val intent = Intent(this@MainActivity,LoginActivity::class.java)
+            var intent: Intent? = null
+            val sh = getSharedPreferences("MySharedPref", MODE_PRIVATE)
+            intent = if(sh.getBoolean("isLogin",false))
+                Intent(this@MainActivity, TrashToTreasure::class.java)
+            else
+                Intent(this@MainActivity, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
