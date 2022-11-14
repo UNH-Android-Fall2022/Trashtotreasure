102b8c88b896c098d1742c1ebac3ab31be4033d2
Neha-reddy <nehareddylomada@gmail.com>
2022-11-14 22:41:23 +0530

    commit for home page changes

--- app/src/main/java/com/newhaven/trashtotreasure/home/TrashToTreasure.kt
+++ app/src/main/java/com/newhaven/trashtotreasure/home/TrashToTreasure.kt
@@ -8,21 +8,21 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.newhaven.trashtotreasure.R
-import com.newhaven.trashtotreasure.databinding.ActivityTrashToTreasure2Binding
+import com.newhaven.trashtotreasure.databinding.ActivityTrashToTreasureBinding
 
-class TrashToTreasureActivity : AppCompatActivity() {
+class TrashToTreasure : AppCompatActivity() {
 
-    private lateinit var binding: ActivityTrashToTreasure2Binding
+    private lateinit var binding: ActivityTrashToTreasureBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityTrashToTreasure2Binding.inflate(layoutInflater)
+        binding = ActivityTrashToTreasureBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_trash_to_treasure2)
+        val navController = findNavController(R.id.nav_host_fragment_activity_trash_to_treasure)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
--- app/src/main/java/com/newhaven/trashtotreasure/home/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/newhaven/trashtotreasure/home/ui/dashboard/DashboardFragment.kt
@@ -9,7 +9,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.newhaven.trashtotreasure.databinding.FragmentDashboardBinding
 
-
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
@@ -29,10 +28,10 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-
-        }
+//        val textView: TextView = binding.textDashboard
+//        dashboardViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
