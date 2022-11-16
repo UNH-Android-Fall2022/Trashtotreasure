c3097e1dfccf4c22c0013eecc8f6a099d517b1b7
Harika <harikagorijala123@gmail.com>
2022-11-16 23:25:52 +0530

    home activity location changes

--- app/src/main/java/com/newhaven/trashtotreasure/home/ui/home/HomeFragment.kt
+++ app/src/main/java/com/newhaven/trashtotreasure/home/ui/home/HomeFragment.kt
@@ -92,8 +92,6 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         savedInstanceState: Bundle?
     ): View {
 
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
