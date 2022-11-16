a89bfbb42f7ee74bd8cb3c0579caab8d7e83e537
Harika <harikagorijala123@gmail.com>
2022-11-16 23:25:26 +0530

    added condition in spalsh

--- app/src/main/java/com/newhaven/trashtotreasure/SplashActivity.kt
+++ app/src/main/java/com/newhaven/trashtotreasure/SplashActivity.kt
@@ -3,7 +3,6 @@ package com.newhaven.trashtotreasure
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.newhaven.trashtotreasure.home.HomeActivity
 import com.newhaven.trashtotreasure.home.TrashToTreasure
 import com.newhaven.trashtotreasure.login.LoginActivity
 import kotlinx.coroutines.CoroutineScope
@@ -12,18 +11,18 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         CoroutineScope(Dispatchers.Main).launch {
             delay(3000)
-            var intent: Intent? = null
+            val intent: Intent?
             val sh = getSharedPreferences("MySharedPref", MODE_PRIVATE)
             intent = if(sh.getBoolean("isLogin",false))
-                Intent(this@MainActivity, TrashToTreasure::class.java)
+                Intent(this@SplashActivity, TrashToTreasure::class.java)
             else
-                Intent(this@MainActivity, LoginActivity::class.java)
+                Intent(this@SplashActivity, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
