eeb9dd5fc514b147fa9e2f8b9873a9f3ee1ddfa9
Neha Reddy <nehareddylomada@gmail.com>
2022-12-01 18:17:30 +0530

    =onContactClick interface updated.

--- app/src/main/java/com/newhaven/trashtotreasure/home/ui/myRequest/onContactUsClick.kt
+++ app/src/main/java/com/newhaven/trashtotreasure/home/ui/myRequest/onContactUsClick.kt
@@ -1,5 +1,11 @@
 package com.newhaven.trashtotreasure.home.ui.myRequest
 
+import java.text.FieldPosition
+
 interface OnContactUsClick {
     fun onClick()
+
+    fun onApproveClick(eid: String, event: HashMap<String, Any>)
+
+    fun onDriverClick(eid: String)
 }
