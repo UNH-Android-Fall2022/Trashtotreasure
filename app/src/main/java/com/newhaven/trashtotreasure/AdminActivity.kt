7b1fbc70fd0a0b5250b083190efe130b8c165181
Harika <harikagorijala123@gmail.com>
2022-12-01 18:11:43 +0530

    created admin activity

---
+++ app/src/main/java/com/newhaven/trashtotreasure/AdminActivity.kt
@@ -0,0 +1,35 @@
+package com.newhaven.trashtotreasure
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.newhaven.trashtotreasure.databinding.ActivityAdminBinding
+
+class AdminActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAdminBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityAdminBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_admin)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_request, R.id.navigation_admin_profile
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
