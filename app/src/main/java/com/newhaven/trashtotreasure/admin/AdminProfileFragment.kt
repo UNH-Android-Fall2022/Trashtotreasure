f3eafc9e18c023b6195ffbc6baa2bc87a8bcbe78
Harika <harikagorijala123@gmail.com>
2022-12-01 18:12:11 +0530

    admin home fragment and profile fragment

--- app/src/main/java/com/newhaven/trashtotreasure/admin/AdminProfileFragment.kt
+++ app/src/main/java/com/newhaven/trashtotreasure/admin/AdminProfileFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [AdminHomeragment.newInstance] factory method to
+ * Use the [AdminProfileFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AdminHomeragment : Fragment() {
+class AdminProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class AdminHomeragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_homeragment, container, false)
+        return inflater.inflate(R.layout.fragment_admin_profile, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class AdminHomeragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment AdminHomeragment.
+         * @return A new instance of fragment AdminProfileFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            AdminHomeragment().apply {
+            AdminProfileFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
