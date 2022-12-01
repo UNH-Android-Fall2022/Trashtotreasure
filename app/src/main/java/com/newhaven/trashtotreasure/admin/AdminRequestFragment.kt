d81af467aaed2430d92b11c0d25be46d96c4064b
Harika <harikagorijala123@gmail.com>
2022-12-01 18:12:33 +0530

    adminrequest list code changes

---
+++ app/src/main/java/com/newhaven/trashtotreasure/admin/AdminRequestFragment.kt
@@ -0,0 +1,175 @@
+package com.newhaven.trashtotreasure.admin
+
+import android.app.Dialog
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.newhaven.trashtotreasure.AdminActivity
+import com.newhaven.trashtotreasure.R
+import com.newhaven.trashtotreasure.databinding.FragmentAdminRequestBinding
+import com.newhaven.trashtotreasure.home.Constants
+import com.newhaven.trashtotreasure.home.TrashToTreasure
+import com.newhaven.trashtotreasure.home.ui.myRequest.Event
+import com.newhaven.trashtotreasure.home.ui.myRequest.OnContactUsClick
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [AdminRequestFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class AdminRequestFragment : Fragment(),OnContactUsClick {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private var _binding: FragmentAdminRequestBinding? = null
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var adapter: AdminRequestListAdapter
+
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAdminRequestBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        getRequestList()
+        return root
+    }
+
+    private fun getRequestList() {
+        binding.progressCircular.visibility = View.VISIBLE
+        var eventList: ArrayList<Event> = ArrayList()
+        FirebaseFirestore.getInstance().collection(Constants.EVENTDETAILS).get()
+            .addOnSuccessListener {
+                for (documents in it.documents) {
+                        if (documents != null) {
+                            Log.d("documentId",documents.id)
+                            eventList.add(
+                                Event(
+                                    documents.data?.get("eid").toString(),
+                                    documents.data?.get("uid").toString(),
+                                    documents.data?.get("name").toString(),
+                                    documents.data?.get("contact").toString(),
+                                    documents.data?.get("address").toString(),
+                                    documents.data?.get("isApproved") as Boolean
+                                )
+                            )
+                        }
+                }
+                binding.progressCircular.visibility = View.GONE
+                linearLayoutManager = LinearLayoutManager(requireContext())
+                binding.rvEventsAdmin.layoutManager = linearLayoutManager
+                adapter = AdminRequestListAdapter(requireContext(),eventList,this)
+                binding.rvEventsAdmin.adapter = adapter
+            }.addOnFailureListener {
+                binding.progressCircular.visibility = View.GONE
+                Toast.makeText(requireContext(), "Failed", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment AdminRequestFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            AdminRequestFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
+    override fun onClick() {
+
+    }
+
+    override fun onApproveClick(eid:String, event: HashMap<String, Any>) {
+        binding.progressCircular.visibility = View.VISIBLE
+
+        FirebaseFirestore.getInstance().collection(Constants.EVENTDETAILS).document(eid).update(
+            event as Map<String, Any>
+        ).addOnSuccessListener {
+            binding.progressCircular.visibility = View.GONE
+            Toast.makeText(context, "Request is approved please refresh page.", Toast.LENGTH_SHORT).show()
+            Log.d("update","updated")
+        }.addOnFailureListener {
+            binding.progressCircular.visibility = View.GONE
+            Toast.makeText(context, "update failed.", Toast.LENGTH_SHORT).show()
+            Log.d("update","update failed")
+        }
+    }
+
+    override fun onDriverClick(eid: String) {
+        showDriverDialog(eid)
+    }
+
+    private fun showDriverDialog(eid: String) {
+
+        val dialog = Dialog(activity as AdminActivity)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.dialog_driver);
+
+        val etName = dialog.findViewById<EditText>(R.id.etDriver)
+        val etPhone = dialog.findViewById<EditText>(R.id.et_mobile)
+        val progressCircular = dialog.findViewById<ProgressBar>(R.id.progress_circular)
+        dialog.findViewById<Button>(R.id.submit).setOnClickListener {
+            progressCircular.visibility = View.VISIBLE
+            val driverDetails = hashMapOf(
+                "eid" to eid,
+                "mobile" to etPhone.text.toString(),
+                "drivername" to etName.text.toString()
+            )
+            progressCircular.visibility = View.VISIBLE
+
+            FirebaseFirestore.getInstance().collection("driverDetails").document().set(driverDetails)
+                .addOnSuccessListener {
+                    Log.d("driverinfo" , "Saved Successfully")
+                    progressCircular.visibility = View.GONE
+                    dialog.dismiss()
+        //                findNavController().navigate(R.id.action_venueDetailsFragment_to_navigation_dashboard)
+                }.addOnFailureListener {
+                progressCircular.visibility = View.GONE
+                Log.d("driverinfo" , "Failed to insert data")
+                dialog.dismiss()
+            }
+        }
+        dialog.show();
+
+        dialog.setCancelable(true)
+    }
+
+}
---
+++ app/src/main/java/com/newhaven/trashtotreasure/admin/AdminRequestListAdapter.kt
@@ -0,0 +1,89 @@
+package com.newhaven.trashtotreasure.admin
+
+import android.content.Context
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.RecyclerView
+import com.newhaven.trashtotreasure.R
+import com.newhaven.trashtotreasure.home.ui.myRequest.Event
+import com.newhaven.trashtotreasure.home.ui.myRequest.OnContactUsClick
+
+class AdminRequestListAdapter(
+    val context: Context,
+    val eventList: List<Event>,
+    val onContactUsClick: OnContactUsClick
+    ) :
+    RecyclerView.Adapter<AdminRequestListAdapter.VHViewHolder>(){
+
+
+    class VHViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val eId: TextView = itemView.findViewById(R.id.tv_reqId)
+        val eName: TextView = itemView.findViewById(R.id.tv_eventName)
+        val eAddress: TextView = itemView.findViewById(R.id.tv_address)
+        val eContact: TextView = itemView.findViewById(R.id.contact_Number)
+        val eStatus: TextView = itemView.findViewById(R.id.tvISApproved)
+        val eDriver: TextView = itemView.findViewById(R.id.status)
+        val tvContact: TextView = itemView.findViewById(R.id.contactUs)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VHViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.row_event_admin, parent, false)
+
+        return VHViewHolder(view)
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onBindViewHolder(holder: VHViewHolder, position: Int) {
+
+
+        val ItemsViewModel = eventList[position]
+
+        // sets the image to the imageview from our itemHolder class
+
+
+        // sets the text to the textview from our itemHolder class
+        holder.eId.text = "${ItemsViewModel.eId}"
+        holder.eName.text = "${ItemsViewModel.name}"
+        holder.eAddress.text = "${ItemsViewModel.address}"
+        holder.eContact.text = "${ItemsViewModel.contact}"
+        if (ItemsViewModel.isApproved) holder.eStatus.text = "Approved" else {
+            holder.eStatus.text = "Approve"
+            holder.eStatus.background = context.getDrawable(R.drawable.btn_red)
+        }
+
+        val eventDetails = hashMapOf(
+            "eid" to ItemsViewModel.eId,
+            "uid" to ItemsViewModel.uId,
+            "name" to ItemsViewModel.name,
+            "contact" to ItemsViewModel.contact,
+            "address" to ItemsViewModel.address,
+            "isApproved" to !ItemsViewModel.isApproved
+        )
+        holder.eStatus.setOnClickListener {
+            onContactUsClick.onApproveClick(ItemsViewModel.eId,eventDetails as HashMap<String, Any> /* = java.util.HashMap<kotlin.String, kotlin.Any> */)
+        }
+
+
+        holder.eDriver.setOnClickListener {
+            if(ItemsViewModel.isApproved){
+                onContactUsClick.onDriverClick(ItemsViewModel.eId)
+            }else{
+                Toast.makeText(context, "Please approve request first.", Toast.LENGTH_SHORT).show()
+            }
+        }
+       // notifyItemChanged(position)
+    }
+
+
+    override fun getItemCount(): Int {
+        return eventList.size
+    }
+
+}
